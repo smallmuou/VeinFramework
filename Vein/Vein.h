0a1cd7e097f0e17cd2f9ca365a4169d380f0ee76
master <lvyexuwenfa100@126.com>
2015-07-29 17:46:17 +0800

    first commit

---
+++ Vein/Vein.h
@@ -0,0 +1,19 @@
+//
+//  Vein.h
+//  Vein
+//
+//  Created by StarNet on 7/29/15.
+//  Copyright (c) 2015 StarNet. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Vein.
+FOUNDATION_EXPORT double VeinVersionNumber;
+
+//! Project version string for Vein.
+FOUNDATION_EXPORT const unsigned char VeinVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Vein/PublicHeader.h>
+
+
